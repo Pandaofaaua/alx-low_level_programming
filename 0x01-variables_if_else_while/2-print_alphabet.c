23448491598df57a16b6d4e311198cf64178cb4e
Pandaofaaua <kvngsheyicarter@gmail.com>
2023-02-17 09:38:14 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Printing character
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char b;
+
+	b = 'a';
+	while
+		(b <= 'z') {
+			putchar(b);
+			b++;
+		}
+	putchar("\n");
+	return (0);
+}
