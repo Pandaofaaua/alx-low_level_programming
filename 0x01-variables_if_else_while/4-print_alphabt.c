48ef95163a0d1056a4ed7dc0a84765902d39df02
Pandaofaaua <kvngsheyicarter@gmail.com>
2023-02-17 12:33:42 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Printing selected alphabets
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char b;
+
+	b = 'a';
+	while
+		(b <= 'z' && != 'q' && != 'p'); {
+			putchar(b);
+			b++;
+		}
+	putchar('\n');
+	return (0);
+}
