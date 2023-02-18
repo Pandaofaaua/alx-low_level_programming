6b4339d318557088703f6a1fb313a68ebd4b92dc
Pandaofaaua <kvngsheyicarter@gmail.com>
2023-02-18 02:19:25 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Printing hexadecimal
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int g;
+
+	i = 0;
+	g = 'a';
+	while
+		(i < 10) {
+			putchar(i + '0');
+			i++;
+		}
+	while
+		(g < 'g') {
+			putchar(g);
+			g++;
+		}
+	putchar('\n);
+	return (0);
+}
