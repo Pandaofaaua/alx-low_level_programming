29b1bf40d90f6c4d82e7487d974be449ccccecb1
Pandaofaaua <kvngsheyicarter@gmail.com>
2023-02-18 01:12:14 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Printing numbers in base ten
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = '0';
+
+	while
+		(i < 10) {
+			putchar(i);
+			i++;
+		}
+	putchar('n');
+	return (0);
+}
