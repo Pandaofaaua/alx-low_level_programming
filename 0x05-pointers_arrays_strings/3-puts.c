899aaff3f2f0394df243cf9dc01876775cfa630c
Pandaofaaua <kvngsheyicarter@gmail.com>
2023-02-27 08:52:04 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print a string followed by a new line to stdout
+ * @str: string to be printed
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
